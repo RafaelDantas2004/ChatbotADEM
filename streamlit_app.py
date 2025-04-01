430936cf7776adebfde822920a48a5a64abab1f4
RafaelDantas2004 <rafaeldantascarv2004@gmail.com>
2025-04-01 12:38:01 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,7 +227,7 @@ else:
     LOGO_BOT = None
 
 # Caminho para o ícone personalizado
-ICON_PATH = "assets/"
+ICON_PATH = "assets/icon_cade.png"
 
 # Verificar se o arquivo do ícone existe
 if os.path.exists(ICON_PATH):
